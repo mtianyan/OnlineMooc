7c12b23c1aba2a73c3faba20f1474f3545529d0f
mtianyan <1147727180@qq.com>
2018-01-09 02:37:29 +0800

    课程机构app：城市，机构，讲师三张数据表书写完毕。对应4-7

--- Readme.md
+++ Readme.md
@@ -1,4 +1,4 @@
-# Mxonline2
+# Mxonline3
 
 [![Build Status](https://travis-ci.org/mtianyan/hexoBlog-Github.svg?branch=master)](https://travis-ci.org/mtianyan/hexoBlog-Github)
 [![MIT Licence](https://badges.frapsoft.com/os/mit/mit.svg?v=103)](https://opensource.org/licenses/mit-license.php)
---
+++ organization/models.py
@@ -0,0 +1,59 @@
+# coding : utf-8
+from datetime import datetime
+
+from django.db import models
+
+# Create your models here.
+
+
+# 城市字典
+class CityDict(models.Model):
+    name = models.CharField(max_length=20, verbose_name=u"城市")
+    # 城市描述：备用不一定展示出来
+    desc = models.CharField(max_length=200, verbose_name=u"描述")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"城市"
+        verbose_name_plural = verbose_name
+
+
+# 课程机构
+class CourseOrg(models.Model):
+    name = models.CharField(max_length=50, verbose_name=u"机构名称")
+    # 机构描述，后面会替换为富文本展示
+    desc = models.TextField(verbose_name=u"机构描述")
+    click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
+    fav_nums = models.IntegerField(default=0, verbose_name=u"收藏数")
+    image = models.ImageField(
+        upload_to="org/%Y/%m",
+        verbose_name=u"封面图",
+        max_length=100)
+    address = models.CharField(max_length=150, verbose_name=u"机构地址")
+    # 一个城市可以有很多课程机构，通过将city设置外键，变成课程机构的一个字段
+    # 可以让我们通过机构找到城市
+    city = models.ForeignKey(CityDict, verbose_name=u"所在城市")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"课程机构"
+        verbose_name_plural = verbose_name
+
+
+# 讲师
+class Teacher(models.Model):
+    # 一个机构会有很多老师，所以我们在讲师表添加外键并把课程机构名称保存下来
+    # 可以使我们通过讲师找到对应的机构
+    org = models.ForeignKey(CourseOrg, verbose_name=u"所属机构")
+    name = models.CharField(max_length=50, verbose_name=u"教师名称")
+    work_years = models.IntegerField(default=0, verbose_name=u"工作年限")
+    work_company = models.CharField(max_length=50, verbose_name=u"就职公司")
+    work_position = models.CharField(max_length=50, verbose_name=u"公司职位")
+    points = models.CharField(max_length=50, verbose_name=u"教学特点")
+    click_nums = models.IntegerField(default=0, verbose_name=u"点击数")
+    fav_nums = models.IntegerField(default=0, verbose_name=u"收藏数")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"教师"
+        verbose_name_plural = verbose_name
