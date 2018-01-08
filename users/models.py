0a59734dd9536f7b9c5967ad62067016853e45cd
mtianyan <1147727180@qq.com>
2018-01-09 00:45:39 +0800

    > Usermodel添加邮箱验证码，首页轮播图。对应4-5

--- users/models.py
+++ users/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from datetime import datetime
+
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
-from django.contrib.auth.models import AbstractUser
 
 
 class UserProfile(AbstractUser):
@@ -13,7 +16,7 @@ class UserProfile(AbstractUser):
     # 昵称
     nick_name = models.CharField(max_length=50, verbose_name=u"昵称", default="")
     # 生日，可以为空
-    birthday = models.DateTimeField(verbose_name=u"生日", null=True, blank=True)
+    birthday = models.DateField(verbose_name=u"生日", null=True, blank=True)
     # 性别 只能男或女，默认女
     gender = models.CharField(
         max_length=5,
@@ -39,3 +42,40 @@ class UserProfile(AbstractUser):
     # 重载__str__方法，打印实例会打印username，username为继承自AbstractUser
     def __str__(self):
         return self.username
+
+# 邮箱验证码model
+
+
+class EmailVerifyRecord(models.Model):
+    SEND_CHOICES = (
+        ("register", u"注册"),
+        ("forget", u"找回密码")
+    )
+    code = models.CharField(max_length=20, verbose_name=u"验证码")
+    # 未设置null = true blank = true 默认不可为空
+    email = models.EmailField(max_length=50, verbose_name=u"邮箱")
+    send_type = models.CharField(choices=SEND_CHOICES, max_length=10)
+    # 这里的now得去掉(),不去掉会根据编译时间。而不是根据实例化时间。
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = "邮箱验证码"
+        verbose_name_plural = verbose_name
+
+# 轮播图model
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u"标题")
+    image = models.ImageField(
+        upload_to="banner/%Y/%m",
+        verbose_name=u"轮播图",
+        max_length=100)
+    url = models.URLField(max_length=200, verbose_name=u"访问地址")
+    # 默认index很大靠后。想要靠前修改index值。
+    index = models.IntegerField(default=100, verbose_name=u"顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"轮播图"
+        verbose_name_plural = verbose_name
